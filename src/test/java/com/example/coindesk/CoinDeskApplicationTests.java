bd97685e3e609f589d81f2cd44126b08ddcf613e
SamLin <L312940@gmail.com>
2023-04-19 22:10:11 +0800

    feat: initial project

---
+++ src/test/java/com/example/coindesk/CoinDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coindesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
