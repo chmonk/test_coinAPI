ae1e308bc95f6a7c4561b96030fbea3d053aa9ad
SamLin <L312940@gmail.com>
2023-04-24 11:25:02 +0800

    fix: correct init data in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 insert into CoinName (en,ch) values ('USD','美元');
 insert into CoinName (en,ch) values ('GBP','英鎊');
-insert into CoinName (en,ch) values ('EUE','歐元');
+insert into CoinName (en,ch) values ('EUR','歐元');
 insert into CoinName (en,ch) values ('NTD','台幣');
